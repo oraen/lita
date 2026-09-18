344a0400ea7ce5aac4b623a0ba16acf0a870e4db
Corki <oraen1998@gmail.com>
2026-09-18 12:46:53 +0800

    m

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+var menuItems = document.querySelectorAll(".menu-item");
+var toast = document.querySelector(".toast");
+var toastTimer = null;
+
+function showComingSoon(title) {
+  toast.textContent = title + "功能正在准备中";
+  toast.classList.add("is-visible");
+
+  window.clearTimeout(toastTimer);
+  toastTimer = window.setTimeout(function () {
+    toast.classList.remove("is-visible");
+  }, 1600);
+}
+
+Array.prototype.forEach.call(menuItems, function (item) {
+  item.addEventListener("click", function () {
+    showComingSoon(item.getAttribute("data-title"));
+  });
+});
